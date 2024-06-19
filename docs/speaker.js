4dbbe219074ea62e8427397b07a3f2d900238152
signalwerk <sh@signalwerk.ch>
2024-06-19 15:11:52 +0200

    MOD: adjust speaker video

--- docs/speaker.js
+++ docs/speaker.js
@@ -3,43 +3,76 @@ class VideoButton extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
-        <style>
-          :host {
-            display: none;
-          }
-          :host(.show) {
-            display: flex;
-          }
-          video {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-          }
-        </style>
-        <video autoplay></video>
-      `;
+          <style>
+            :host {
+              display: none;
+            }
+            :host(.show) {
+              display: flex;
+            }
+            video {
+              width: 100%;
+              height: 100%;
+              object-fit: cover;
+            }
+          </style>
+          <video autoplay></video>
+        `;
+    this.stream = null;
   }
 
   connectedCallback() {
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then((stream) => {
-        this.shadowRoot.querySelector("video").srcObject = stream;
-        // this.classList.add("show");
-      })
-      .catch((error) => {
-        console.error("Error accessing the camera", error);
-      });
+    if (this.classList.contains("show")) {
+      this.startVideoStream();
+    }
 
     document.addEventListener("keydown", this.handleKeydown.bind(this));
   }
 
+  disconnectedCallback() {
+    document.removeEventListener("keydown", this.handleKeydown.bind(this));
+    this.stopVideoStream();
+  }
+
   handleKeydown(event) {
     if (event.ctrlKey && event.key === "h") {
+      this.startVideoStream(); // Start the video stream but never stop it (fast access to the camera)
       this.classList.toggle("show");
     } else if (event.ctrlKey && event.key === "f") {
+      this.startVideoStream();
       this.classList.toggle("big");
       this.classList.add("show");
+    }
+  }
+
+  toggleVideoStream() {
+    if (this.classList.contains("show")) {
+      this.startVideoStream();
+    } else {
+      this.stopVideoStream();
+    }
+  }
+
+  startVideoStream() {
+    if (!this.stream) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          this.stream = stream;
+          this.shadowRoot.querySelector("video").srcObject = stream;
+        })
+        .catch((error) => {
+          console.error("Error accessing the camera", error);
+        });
+    }
+  }
+
+  stopVideoStream() {
+    if (this.stream) {
+      const tracks = this.stream.getTracks();
+      tracks.forEach((track) => track.stop());
+      this.shadowRoot.querySelector("video").srcObject = null;
+      this.stream = null;
     }
   }
 }
